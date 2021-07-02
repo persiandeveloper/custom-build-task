1dcc5b88388f4e8ff96b6a81164f13bc0187a292
persiandeveloper <b.fakhriloo@gmail.com>
2021-07-02 11:26:33 +0100

    Init

---
+++ SnykTask/SnykTaskFile.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+using System.Diagnostics;
+
+namespace SnykTask
+{
+    public class SnykTaskFile : Task
+    {
+        public override bool Execute()
+        {
+            var processInfo = new ProcessStartInfo();
+            processInfo.FileName = "cmd";
+            Process.Start(processInfo);
+
+            return true;
+        }
+    }
+}
