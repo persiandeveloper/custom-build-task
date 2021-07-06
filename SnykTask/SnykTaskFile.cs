39892fdf7c03c5eba83a0a0cc03a5b24c4da9a8f
persiandeveloper <b.fakhriloo@gmail.com>
2021-07-06 11:37:14 +0100

    added location

--- SnykTask/SnykTaskFile.cs
+++ SnykTask/SnykTaskFile.cs
@@ -10,9 +10,12 @@ namespace SnykTask
         {
             var processInfo = new ProcessStartInfo();
             processInfo.FileName = "cmd";
+            processInfo.WorkingDirectory = Location;
             Process.Start(processInfo);
 
             return true;
         }
+
+        public string Location { get; set; }
     }
 }
